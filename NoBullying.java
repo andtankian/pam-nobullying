0e9f39be6c91ec55bc9f5fb61bcd931712f3ce27
ANDREW RIBEIRO <ANDREW.RIBEIRO@alunos.etec.itaqua>
2019-03-14 22:04:28 -0300

    Correção, adicionado passo para cada etapa

--- NoBullying.java
+++ NoBullying.java
@@ -1,15 +1,15 @@
 public class NoBullying {
   public static void main(String[] args){
     System.out.println("Programa NoBullying");
-    System.out.println("Destinação de maior verba para auxilio psicológico nas escolas");
-    System.out.println("Abordar o tema em escolas");
-    System.out.println("Conversar com pessoas que sofrem Bullying");
-    System.out.println("Realizar campanhas anti-bullying");
-    System.out.println("Mais segurança nas escolas, aumentar ronda-escolar.");
-    System.out.println("Respeitar as diferenças das pessoas");
-    System.out.println("Conversar com os responsáveis, caso seja vítima");
-    System.out.println("Manter-se fora de grupos de zoação.");
-    System.out.println("Ter empatia pelo próximo.");
-    System.out.println("Não julgar as pessoas pelos seus atos, apenas ouvi-las");
+    System.out.println("Passo 1 Destinação de maior verba para auxilio psicológico nas escolas");
+    System.out.println("Passo 2 Abordar o tema em escolas");
+    System.out.println("Passo 3 Conversar com pessoas que sofrem Bullying");
+    System.out.println("Passo 4 Realizar campanhas anti-bullying");
+    System.out.println("Passo 5 Mais segurança nas escolas, aumentar ronda-escolar.");
+    System.out.println("Passo 6 Respeitar as diferenças das pessoas");
+    System.out.println("Passo 7 Conversar com os responsáveis, caso seja vítima");
+    System.out.println("Passo 8 Manter-se fora de grupos de zoação.");
+    System.out.println("Passo 9 Ter empatia pelo próximo.");
+    System.out.println("Passo 10 Não julgar as pessoas pelos seus atos, apenas ouvi-las");
   }
 }
