5c85eb9ccc4db30ad00e76b7ed38592a0bfbfb10
ANDREW RIBEIRO <ANDREW.RIBEIRO@alunos.etec.itaqua>
2019-03-14 21:54:48 -0300

    Escrevi quarto passo, sobre algumas campanhas

--- NoBullying.java
+++ NoBullying.java
@@ -4,5 +4,6 @@ public class NoBullying {
     System.out.println("Destinação de maior verba para auxilio psicológico nas escolas");
     System.out.println("Abordar o tema em escolas");
     System.out.println("Conversar com pessoas que sofrem Bullying");
+    System.out.println("Realizar campanhas anti-bullying");
   }
 }
