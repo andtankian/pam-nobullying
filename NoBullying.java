df18f65e2a8db1e9bb430b8786d7b86a6c3239f8
ANDREW RIBEIRO <ANDREW.RIBEIRO@alunos.etec.itaqua>
2019-03-14 21:54:01 -0300

    Escrevi o terceiro passo, sobre conversa com vítimas

--- NoBullying.java
+++ NoBullying.java
@@ -3,5 +3,6 @@ public class NoBullying {
     System.out.println("Programa NoBullying");
     System.out.println("Destinação de maior verba para auxilio psicológico nas escolas");
     System.out.println("Abordar o tema em escolas");
+    System.out.println("Conversar com pessoas que sofrem Bullying");
   }
 }
