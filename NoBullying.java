8f0404baaa0357f1cc0b0eaf5d8d8b4fe737fedc
ANDREW RIBEIRO <ANDREW.RIBEIRO@alunos.etec.itaqua>
2019-03-14 21:51:07 -0300

    Escrevi o primeiro passo do programa

--- NoBullying.java
+++ NoBullying.java
@@ -1,5 +1,6 @@
 public class NoBullying {
   public static void main(String[] args){
     System.out.println("Programa NoBullying");
+    System.out.println("Destinação de maior verba para auxilio psicológico nas escolas");
   }
 }
