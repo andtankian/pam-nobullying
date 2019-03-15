5d8110d452b53e5e2b47f519081c86305d23ed20
ANDREW RIBEIRO <ANDREW.RIBEIRO@alunos.etec.itaqua>
2019-03-14 21:52:28 -0300

    Escrevi segundo passo sobre abordagem nas escolas

--- NoBullying.java
+++ NoBullying.java
@@ -2,5 +2,6 @@ public class NoBullying {
   public static void main(String[] args){
     System.out.println("Programa NoBullying");
     System.out.println("Destinação de maior verba para auxilio psicológico nas escolas");
+    System.out.println("Abordar o tema em escolas");
   }
 }
